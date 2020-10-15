1c8125b3aacdb2d9609d8f6472ffa34ee9f8774c
Tim Metzler <tim.metzler@h-brs.de>
2020-10-15 14:34:27 +0200

    Load config from jupyter_paths

--- e2xhelp/server_extensions/help/help.py
+++ e2xhelp/server_extensions/help/help.py
@@ -1,27 +1,34 @@
 import os
 from tornado import web
 from notebook.utils import url_path_join as ujoin
+from jupyter_core.paths import jupyter_config_path
+from traitlets.config.loader import PyFileConfigLoader
 from traitlets.config import LoggingConfigurable, Config
 from traitlets import Unicode, Bool
 from textwrap import dedent
-from ...config import ConfigManager
 
 class Help(LoggingConfigurable):
 
-    static_path = Unicode(os.path.join(os.path.dirname(__file__), 'static'), help=dedent(
-        'path to static files shipped with package'
-    ))
+    static_path = Unicode(os.path.join(os.path.dirname(__file__), 'static'), 
+        help='path to static files shipped with package'
+    )
 
-    use_shared_path = Bool(False, help=dedent(
-        'Whether to use static files from the shared_path'
-    )).tag(config=True)
-
-    shared_path = Unicode('', help=dedent(
-        'path to extra files served under /e2xhelp/shared'
-    )).tag(config=True)
+    shared_path = Unicode(None, 
+        help='path to extra files served under /e2xhelp/shared',
+        allow_none=True
+    ).tag(config=True)
 
     def __init__(self):
-        self.config = ConfigManager().get_config()
+        self.config = self.load_config('e2xhelp_config.py')
+
+    def load_config(self, config_name):
+        cfg = Config()
+        for path in jupyter_config_path():
+            file = os.path.join(path, config_name)
+            if os.path.isfile(file):
+                new_cfg = PyFileConfigLoader(file).load_config()
+                cfg.merge(new_cfg)
+        return cfg
 
     def get_handlers(self):
         handlers = [
@@ -30,7 +37,7 @@ class Help(LoggingConfigurable):
                 'default_filename': 'index.html'
             })
         ]
-        if self.use_shared_path:
+        if self.shared_path:
             handlers.append(
                 (r"/e2xhelp/shared/(.*)", web.StaticFileHandler, {
                     'path': self.shared_path, 
