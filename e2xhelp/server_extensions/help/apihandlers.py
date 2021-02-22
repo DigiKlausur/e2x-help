32a7b4b9031bffd961820527545f95c3365800b6
Tim Metzler <tim.metzler@h-brs.de>
2021-02-22 20:43:34 +0100

    Filter out subdirs with an index.html

--- e2xhelp/server_extensions/help/apihandlers.py
+++ e2xhelp/server_extensions/help/apihandlers.py
@@ -11,9 +11,9 @@ class ListFilesHandler(IPythonHandler):
             return []
         file_list = []
         exclude_dirs = []
-        
+
         for root, dirs, files in os.walk(path):
-            root = os.path.relpath(root, path)
+            
             for name in dirs:
                 if name.startswith('.'):
                     exclude_dirs.append(name)
@@ -22,11 +22,14 @@ class ListFilesHandler(IPythonHandler):
                     file_list.append((os.path.join(root, name), os.path.join(root, name, 'index.html')))
 
             dirs[:] = [d for d in dirs if d not in exclude_dirs]
+
             for name in files:
                 if name == 'index.html':
                     exclude_dirs.append(dirs)
                 file_list.append((os.path.join(root, name), os.path.join(root, name)))
-                
+        
+        file_list = [(os.path.relpath(name, path), os.path.relpath(url, path)) for name, url in file_list]
+
         return file_list
 
     @web.authenticated
